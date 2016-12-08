4ed317f7c0c603e34f10ee3d012b11afb4720ab8
Jackzhy <13816178481@163.com>
2016-12-08 01:14:22 -0500

    Create package.js

---
+++ final-dmo/package.js
@@ -0,0 +1,27 @@
+{
+  "name": "411",
+  "version": "1.0.1",
+  "description": "food truck",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "zhy",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.15.2",
+    "ejs": "^2.5.3",
+    "express": "^4.14.0",
+    "mongodb": "^2.2.12"
+  },
+  "devDependencies": {},
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Jackzhy/software-engineering.git"
+  },
+  "bugs": {
+    "url": "https://github.com/Jackzhy/software-engineering/issues"
+  },
+  "homepage": "https://github.com/Jackzhy/software-engineering#readme"
+}
