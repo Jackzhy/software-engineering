d0744da276a79a4dbcf1a2219c2a69a42c46363b
Jackzhy <13816178481@163.com>
2016-12-21 21:13:05 -0500

    Update server.js

--- final-dmo/server.js
+++ final-dmo/server.js
@@ -3,13 +3,6 @@ const app = express();
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-
-
-
-
-
-
-
 var db
 
 MongoClient.connect('mongodb://localhost:27017/repo', (err, database) => {
@@ -25,21 +18,44 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+
+function filter(data){
+  var dayindex = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  var day = dayindex[new Date().getDay() - 1 ];
+  var hour = new Date().getHours();
+  var time = "Breakfast";
+  if(hour >= 10 ){
+    time = "Lunch";
+    if(hour >= 15){
+      time = "Dinner";
+    }
+  }
+  var r = [];
+  for (i = 0; i < data.length; i++) {
+    if(data[i].day == day && data[i].time == time){
+      r.push(data[i]);
+    }
+  }
+  return r;
+}
+
 app.get('/', (req, res) => {
 //  res.sendFile('/Users/hongyuzhou/Desktop/411/project' + '/goog.html')
   db.collection("411.foodTruckSchedual").find().toArray((err, result) => {
     if (err) return console.log(err)
-//    console.log(result)
+
+    result = filter(result);
+//    result.push()
+//    console.log(filter(result))
     res.render('index.ejs', {tdata: result})
   })
 })
 
 app.post('/tdata', (req, res) => {
+
   db.collection("411.foodTruckSchedual").find(req.body).toArray((err, result) => {
     if (err) return console.log(err)
-//    console.log('saved to database')
     res.render('index.ejs', {tdata: result})
   })
 })
-
 // Note: request and response are usually written as req and res respectively.
